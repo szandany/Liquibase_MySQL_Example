8355e842b3144cfe5c9ef3ff2136d90509f5a462
szandany <szandany@datical.com>
2021-08-26 11:42:37 -0500

    added new create table department

--- changelog.sql
+++ changelog.sql
@@ -1,3 +1,11 @@
 -- liquibase formatted sql changeLogId:801204f4-7e7d-4f93-9c83-83f7a1d02e06
 
-
+-- changeset SteveZ:1588216
+CREATE TABLE department 
+(
+  id INT NOT NULL, 
+  name VARCHAR(50) NOT NULL, 
+  active BIT(1) DEFAULT 1 NULL, 
+  CONSTRAINT PK_DEPARTMENT PRIMARY KEY (id))
+  ;
+--rollback drop table department;
